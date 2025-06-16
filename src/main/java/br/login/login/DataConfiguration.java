94f845ae2c749ed3ca4b4218e4d5157e70902ccd
terradd <reinaldopaixaodev@gmail.com>
2025-06-15 22:21:25 -0400

    annotation

--- src/main/java/br/login/login/DataConfiguration.java
+++ src/main/java/br/login/login/DataConfiguration.java
@@ -2,14 +2,38 @@ package br.login.login;
 
 import javax.sql.DataSource;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
 
 public class DataConfiguration {
+
+    @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/login?useTimezone=true&serverTimezone-UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("dinossauro30"); 
+        return dataSource();
+    }
+        @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MariaDBDialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
+
+        }
 
     }
 
-}
+
